6428dda160320b49b9b6473f8fec8835de34d61d
Camilo Urrego <urrego@outlook.com>
2020-10-13 13:03:03 -0500

    Se extiende la clase principal ,esperando que sirva en aws

--- src/main/java/com/aws/codestar/projecttemplates/HelloWorldApplication.java
+++ src/main/java/com/aws/codestar/projecttemplates/HelloWorldApplication.java
@@ -2,9 +2,10 @@ package com.aws.codestar.projecttemplates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
