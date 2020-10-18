598d4ba1d24d14bf28479df7aacf3d03adccee30
Camilo Urrego <urrego@outlook.com>
2020-10-17 21:05:28 -0500

    Primera implementacion de colas, busca una cola y si no existe la crea omg

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,18 +1,57 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
+import software.amazon.awssdk.services.sqs.model.ListQueuesResponse;
+
+import java.net.URI;
 
 @RestController
 public class HelloWorldController {
+    @Value("${aws.local.endpoint}")
+    String awsEndpoint;
+
+    @Value("${aws.sqs.name}")
+    String queueName;
+
+    Region REGION = Region.US_EAST_1;
 
     @RequestMapping(method = RequestMethod.GET)
     public String helloWorld() {
 
-        return "Supp";
+        SqsClient sqsClient;
+
+        if (awsEndpoint != null) {
+            sqsClient = SqsClient.builder()
+                    .region(REGION)
+                    .endpointOverride(URI.create(awsEndpoint))
+                    .build();
+        } else {
+            sqsClient = SqsClient.builder()
+                    .region(REGION)
+                    .build();
+        }
+
+
+        ListQueuesRequest listQueuesRequest = ListQueuesRequest.builder()
+                .queueNamePrefix(queueName).build();
+        ListQueuesResponse listQueuesResponse = sqsClient.listQueues(listQueuesRequest);
+
+        if (!listQueuesResponse.hasQueueUrls()) {
+            CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
+                    .queueName(queueName)
+                    .build();
+
+            sqsClient.createQueue(createQueueRequest);
+        }
+
+        return listQueuesResponse.toString();
     }
 
 }
